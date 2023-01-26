d876fbebc0c2dc0a4cc1c85c0467d27b87b41245
DungDang25 <dungdang25@hotmail.com>
2023-01-26 16:25:04 +1000

    start volume control

--- Handtracking Vision/New.py
+++ Handtracking Vision/New.py
@@ -1,58 +1,26 @@
 import cv2 as cv 
 import mediapipe as mp
 import time
+import HandTrackingModule as HTM
 
-class handDetector():
-    def __init__(self, mode=False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
-        self.mode = mode
-        self.maxHands = maxHands
-        self.detectionCon = detectionCon
-        self.trackCon = trackCon
+pTime = 0
+cTime = 0
+cap = cv.VideoCapture(0)
+detector = HTM.handDetector()
 
-        self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
-        self.mpDraw = mp.solutions.drawing_utils
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    
+    if len(lmList) != 0:
+        print(lmList[0])
 
-    def findHands(self, img, draw=True):
-        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
-        ## print(results.multi_hand_landmarks)
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
 
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
-                if draw:
-                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+    cv.putText(img, str(int(fps)),(10,70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
-                # For hand landmark coordinates
-                # for id, lm in enumerate(handLms.landmark):
-                #     ##print(id, lm)
-                #     h, w, c= img.shape
-                #     cx, cy = int(lm.x*w), int(lm.y*h)
-                #     print(id, cx, cy)
-                #     if id == 0:
-                #         cv.circle(img, (cx, cy), 10, (255, 0, 255), cv.FILLED)
-        return img
-
-def main():
-    pTime = 0
-    cTime = 0
-    cap = cv.VideoCapture(0)
-
-    detector = handDetector()
-
-    while True:
-        success, img = cap.read()
-        img = detector.findHands(img)
-
-        cTime = time.time()
-        fps = 1/(cTime-pTime)
-        pTime = cTime
-
-        cv.putText(img, str(int(fps)),(10,70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-        cv.imshow("Image", img)
-        cv.waitKey(1)
-
-
-if __name__ == "__main__":
-    main()
+    cv.imshow("Image", img)
+    cv.waitKey(1)
