027f8f595db4819484883863928ba16943b58f1b
DungDang25 <dungdang25@hotmail.com>
2023-01-26 14:17:27 +1000

    temp changes

--- Handtracking Vision/New.py
+++ Handtracking Vision/New.py
@@ -2,39 +2,57 @@ import cv2 as cv
 import mediapipe as mp
 import time
 
-cap = cv.VideoCapture(0)
+class handDetector():
+    def __init__(self, mode=False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
 
-pTime = 0
-cTime = 0
+    def findHands(self, img, draw=True):
+        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        results = self.hands.process(imgRGB)
+        ## print(results.multi_hand_landmarks)
 
-while True:
-    success, img = cap.read()
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
 
-    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+                # For hand landmark coordinates
+                # for id, lm in enumerate(handLms.landmark):
+                #     ##print(id, lm)
+                #     h, w, c= img.shape
+                #     cx, cy = int(lm.x*w), int(lm.y*h)
+                #     print(id, cx, cy)
+                #     if id == 0:
+                #         cv.circle(img, (cx, cy), 10, (255, 0, 255), cv.FILLED)
+        return img
 
-    print(results.multi_hand_landmarks)
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv.VideoCapture(0)
 
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-            for id, lm in enumerate(handLms.landmark):
-                ##print(id, lm)
-                h, w, c= img.shape
-                cx, cy = int(lm.x*w), int(lm.y*h)
-                print(id, cx, cy)
-                if id == 0:
-                    cv.circle(img, (cx, cy), 10, (255, 0, 255), cv.FILLED)
+    detector = handDetector()
 
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
 
-    cv.putText(img, str(int(fps)),(10,70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
 
-    cv.imshow("Image", img)
-    cv.waitKey(1)
+        cv.putText(img, str(int(fps)),(10,70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        cv.imshow("Image", img)
+        cv.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
